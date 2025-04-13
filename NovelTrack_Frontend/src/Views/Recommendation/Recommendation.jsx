ea286377b4c82ff0f46ac5510816287592865b3f
elitekingfisher <asifshayan003@gmail.com>
2025-04-13 06:38:07 -0500

    Update Recommendation.jsx

--- NovelTrack_Frontend/src/Views/Recommendation/Recommendation.jsx
+++ NovelTrack_Frontend/src/Views/Recommendation/Recommendation.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+const Recommendation = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold mb-4">Recommended Books</h1>
+      <p>Here are your recommended books.</p>
+    </div>
+  );
+};
+
+export default Recommendation;
