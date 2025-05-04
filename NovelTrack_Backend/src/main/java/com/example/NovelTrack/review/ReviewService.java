ba3a721ebaec21d977155a83be911818bb0ccafe
elitekingfisher <asifshayan003@gmail.com>
2025-05-03 19:02:25 -0500

    Update ReviewService.java

--- NovelTrack_Backend/src/main/java/com/example/NovelTrack/review/ReviewService.java
+++ NovelTrack_Backend/src/main/java/com/example/NovelTrack/review/ReviewService.java
@@ -2,11 +2,9 @@ package com.example.NovelTrack.review;
 
 import com.example.NovelTrack.exception.InvalidPermissionsException;
 import com.example.NovelTrack.exception.ResourceNotFoundException;
-import com.example.NovelTrack.trackitem.TrackItem;
-import com.example.NovelTrack.trackitem.TrackItemDTO;
 import com.example.NovelTrack.user.User;
-import com.example.NovelTrack.user.UserMapper;
 import com.example.NovelTrack.user.UserRepository;
+import com.example.NovelTrack.user.UserMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,59 +17,72 @@ import java.util.stream.Collectors;
 @Service
 @AllArgsConstructor
 public class ReviewService {
-    private ReviewRepository reviewRepository;
-    private UserRepository userRepository;
-    public List<ReviewDTO> getAllReviews()
-    {
-        List<Review> reviews =  reviewRepository.findAll();
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+
+    public List<ReviewDTO> getAllReviews() {
+        List<Review> reviews = reviewRepository.findAll();
         return reviews.stream().map(this::toDTO).collect(Collectors.toList());
     }
 
-    public List<ReviewDTO> getAllReviewsByUser(Long userId)
-    {
+    public List<ReviewDTO> getAllReviewsByUser(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
-        List<Review> reviews =  reviewRepository.findAllByUser(user);
+        List<Review> reviews = reviewRepository.findAllByUser(user);
         return reviews.stream().map(this::toDTO).collect(Collectors.toList());
     }
 
-    public List<ReviewDTO> getAllReviewsForBook(String bookId)
-    {
-        List<Review> reviews =  reviewRepository.findAllByBookId(bookId);
+    public List<ReviewDTO> getAllReviewsForBook(String bookId) {
+        List<Review> reviews = reviewRepository.findAllByBookId(bookId);
         return reviews.stream().map(this::toDTO).collect(Collectors.toList());
     }
 
-    public ReviewDTO getReview(Long id)
-    {
+    public ReviewDTO getReview(Long id) {
         Review review = reviewRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Review not found with given id:" + id));
         return this.toDTO(review);
     }
 
-    public List<ReviewDTO> getAllReviewsForBookByUser(Long userId, String bookId)
-    {
+    public List<ReviewDTO> getAllReviewsForBookByUser(Long userId, String bookId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
-        List<Review> reviews =  reviewRepository.findByUserAndBookId(user, bookId);
+        List<Review> reviews = reviewRepository.findByUserAndBookId(user, bookId);
         return reviews.stream().map(this::toDTO).collect(Collectors.toList());
     }
 
-    public ReviewDTO createReview(Long userId, ReviewRequest reviewRequest)
-    {
+    // Create a default user if one doesn't exist
+    public ReviewDTO createReview(Long userId, ReviewRequest reviewRequest) {
         Review review = new Review();
+
+        
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+                .orElseGet(() -> createDefaultUser());
+
         review.setUser(user);
         review.setBookId(reviewRequest.getBookId());
         review.setBookTitle(reviewRequest.getBookTitle());
         review.setBookImageUrl(reviewRequest.getBookImageUrl());
         review.setContent(reviewRequest.getContent());
         review.setLastChanged(LocalDateTime.now());
+
         return this.toDTO(reviewRepository.save(review));
     }
 
-    private ReviewDTO toDTO(Review review)
-    {
+
+    
+    private User createDefaultUser() {
+        User defaultUser = new User();
+        defaultUser.setEmail("guest@example.com");
+        defaultUser.setUsername("GuestUser");
+        defaultUser.setPassword("guestpassword");  
+        defaultUser.setDescription("Guest user with no specific description");
+
+        return userRepository.save(defaultUser);
+    }
+
+
+
+    private ReviewDTO toDTO(Review review) {
         ReviewDTO reviewDTO = new ReviewDTO();
         reviewDTO.setUser(UserMapper.mapToUserDTO(review.getUser()));
         reviewDTO.setId(review.getId());
@@ -83,36 +94,37 @@ public class ReviewService {
         return reviewDTO;
     }
 
-    public ReviewDTO editReview(Long userId, Long reviewId, ReviewRequest reviewRequest)
-    {
+    public ReviewDTO editReview(Long userId, Long reviewId, ReviewRequest reviewRequest) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
-        Review review = reviewRepository.findById(reviewId).
-                orElseThrow(() -> new ResourceNotFoundException("Review not found with id: " + reviewId));
-        if(!Objects.equals(review.getUser().getId(), userId))
-        {
-            throw new ResourceNotFoundException("Review not found with user id: " + userId + "and book id: " + reviewRequest.getBookId());
+
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ResourceNotFoundException("Review not found with id: " + reviewId));
+
+        if (!Objects.equals(review.getUser().getId(), userId)) {
+            throw new InvalidPermissionsException("Review does not belong to user id: " + userId);
         }
+
         review.setContent(reviewRequest.getContent());
         review.setLastChanged(LocalDateTime.now());
+
         return this.toDTO(reviewRepository.save(review));
     }
 
     @Transactional
-    public void deleteReview(Long id, Long userId)
-    {
+    public void deleteReview(Long id, Long userId) {
         Review review = reviewRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Review does not exist with the given id: " + id));
-        if(!Objects.equals(review.getUser().getId(), userId))
-        {
-            throw new InvalidPermissionsException("Review not found with user id: " + userId + "and book id: " + review.getBookId());
+
+        if (!Objects.equals(review.getUser().getId(), userId)) {
+            throw new InvalidPermissionsException("Review does not belong to user id: " + userId);
         }
+
         reviewRepository.delete(review);
     }
 
     @Transactional
-    public void deleteAllReviewsByUser(Long userId)
-    {
+    public void deleteAllReviewsByUser(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
         reviewRepository.deleteByUser(user);  // Deleting by User
